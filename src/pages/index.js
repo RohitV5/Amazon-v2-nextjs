a655682af147d26b835f93f16310046aca95e059
Rohit Verma <verma.rohit.in@gmail.com>
2023-02-22 09:20:46 +0530

    chore: init template

---
+++ src/pages/index.js
@@ -0,0 +1,17 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Amazon 2.0</title>
+      </Head>
+
+      {/* ---- TO BEGIN, delete this section and GET CODING!!! ---- */}
+      <center className="grid place-items-center mt-10">
+        <h1 className="text-5xl">Lets build Amazon 2.0</h1>
+      </center>
+      {/* ---- ---- */}
+    </div>
+  );
+}
